8916f4256bfa5f2d5a41df822214c3f7c35a9459
hendcr18 <55844963+hendcr18@users.noreply.github.com>
2020-08-27 13:45:34 -0400

    Update master.sh

--- master.sh
+++ master.sh
@@ -50,8 +50,6 @@ systemctl restart kubelet
 
 sudo kubeadm init --pod-network-cidr=192.168.0.0/16
 
-exit
-
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
