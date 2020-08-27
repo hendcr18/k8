a69502423d8698ffa9361743089ccd12f33e8aec
hendcr18 <55844963+hendcr18@users.noreply.github.com>
2020-08-27 14:55:13 -0400

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-
-kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
-
-kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
