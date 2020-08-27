209b63d2b57eeb5bb0edb3a079fd6ce486746557
hendcr18 <55844963+hendcr18@users.noreply.github.com>
2020-08-27 14:11:33 -0400

    Update master.sh

--- master.sh
+++ master.sh
@@ -49,11 +49,3 @@ systemctl restart kubelet
 
 
 kubeadm init --pod-network-cidr=192.168.0.0/16
-
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-
-kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
-
-kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
